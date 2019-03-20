95cead54981b3787831e8cb63873294591e9e7f9
Jagdeep Singh <jagdeeeeep@gmail.com>
2019-03-19 21:48:16 -0700

    added TODO tasks

--- server.js
+++ server.js
@@ -14,23 +14,18 @@ const PORT = process.env.PORT;
 
 // location route, returns location object
 // Keys: search_query, formatted_query, latitude and longitude
-app.get('/location', searchToLatLong);
-
-// (req, res) => {
-//   if (req.query.data.length === 0) {
-//     res.status(500).send('Invalid Location');
-//   } else { 
-//     const locationData = searchToLatLong(req.query);
-//     res.send(locationData);
-//   }
-// });
+app.get('/location', getLocation);
 
 // weather route, returns an array of forecast objects
 // Keys: forecast, time
-app.get('/weather', (req, res) => {
-  const forecastData = searchToDailyForecast(req.query);
-  res.send(forecastData);
-});
+app.get('/weather', getWeather);
+
+// TODO: create a getMeetups function
+// app.get('/meetups', getMeetups);
+
+// TODO: create a getYelp function
+// app.get('/yelp', getYelp);
+
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
@@ -42,19 +37,25 @@ function handleError(err, res) {
 // HELPER FUNCTIONS
 
 // takes search request and convert to location object
-
-// TODO: refactor this function
-// function searchToLatLong(query) {
-//   const geoData = require('./data/geo.json');
-//   const location = new Location(geoData, Object.values(query)[0]);
-//   return location;
-// }
-
-function searchToLatLong(req, res) {
+function getLocation(req, res) {
   const mapsURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_MAPS_API_KEY}&address=${req.query.data}`;
   return superagent.get(mapsURL)
     .then(result => {
       res.send(new Location(result.body.results[0], req.query));
+    })
+    .catch(error => handleError(error));
+}
+
+// returns array of daily forecasts
+function getWeather(req, res) {
+  const dark_sky_url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${req.query.data.latitude},${req.query.data.longitude}`;
+  
+  return superagent.get(dark_sky_url)
+    .then( weatherResult => {
+      const weatherSummaries = weatherResult.body.daily.data.map((day,i) => {
+        return new Forecast(day);
+      });
+      res.send(weatherSummaries);
     })
     .catch(error => handleError(error));
 }
@@ -68,21 +69,8 @@ function Location(data, query) {
   this.longitude = data.geometry.location.lng;
 }
 
-// returns array of daily forecasts
-function searchToDailyForecast(query) {
-  const weatherData = require('./data/darksky.json');
-  const dailyForecast = weatherData.daily.data;
-  let result = [];
-
-  dailyForecast.forEach(day => {
-    let time = new Date(day.time*1000).toString().slice(0,15);
-    result.push(new Forecast(day.summary, time));
-  });
-  return result;
-}
-
 // Forecast object constructor
-function Forecast(forecast, time) {
-  this.forecast = forecast;
-  this.time = time;
+function Forecast(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time*1000).toString().slice(0,15);
 }
